9c3ee2e9015d0eac6a45e10ea5b90ca45466ff6d
Ricardo Gallegos <gallegos.rickgalle@gmail.com>
2025-04-24 15:00:30 -0600

    dr-0006 fix responsive and update the imagenes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,10 +14,10 @@ export const Header = (props) => {
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
-                  href="#features"
+                  href="#contact"
                   className="btn btn-custom btn-lg page-scroll"
                 >
-                  Aprende M&aacute;s
+                  Contactanos
                 </a>{" "}
               </div>
             </div>
